230bcf52aeff2bc732dc6603d96f93074c0d89d4
hivongdc-star <hivongdc@gmail.com>
2025-08-29 14:38:41 +0900

    Update: đồng bộ stats, sửa profile command, fix elemen

--- utils/element.js
+++ utils/element.js
@@ -1,10 +1,10 @@
 // element.js
 module.exports = {
-  kim: { armor: 2, defense: 1, hp: 5 },
-  moc: { mana: 3, attack: 1, hp: 3 },
-  thuy: { mana: 4, hp: 5, defense: 1 },
-  hoa: { attack: 4, hp: 3 },
-  tho: { defense: 3, armor: 2, hp: 7 },
+  kim: { hp: 6, mana: 2, attack: 2, defense: 3, armor: 3 }, // Trâu, thiên về thủ + giáp
+  moc: { hp: 5, mana: 5, attack: 3, defense: 2, armor: 2 }, // Cân bằng, thiên về mana
+  thuy: { hp: 7, mana: 4, attack: 2, defense: 3, armor: 2 }, // Nhiều máu, mana ổn định
+  hoa: { hp: 5, mana: 2, attack: 5, defense: 2, armor: 2 }, // Công cao, máu vừa
+  tho: { hp: 8, mana: 2, attack: 2, defense: 4, armor: 4 }, // Tank cứng, thủ + giáp cao
 };
 
 // Map hiển thị
